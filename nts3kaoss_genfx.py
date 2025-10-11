fb8332f833c232192391026da61ea6fad13dad57
boochow <boochowp@gmail.com>
2025-10-11 14:37:15 +0900

    fix class name, add touch event related builtin parameters

--- nts3kaoss_genfx.py
+++ nts3kaoss_genfx.py
@@ -1,7 +1,8 @@
 from hvccloguesdk import LogueSDKV2Generator
 
-class Nts1mkii_del(LogueSDKV2Generator):
+class Nts3_genfx(LogueSDKV2Generator):
     FIXED_PARAMS = ()
+    BUILTIN_PARAMS = ("touch_began", "touch_moved", "touch_ended", "touch_stationary", "touch_cancelled")
     UNIT_NUM_OUTPUT = 2
     MAX_SDRAM_SIZE = 3145728
     MAX_UNIT_SIZE = 32768
