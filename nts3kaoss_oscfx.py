2c83a2fc461b114c2464cca82dc06aa5ef86579f
boochow <boochowp@gmail.com>
2025-10-18 17:40:52 +0900

    add built-in variables for touch pad events

--- nts3kaoss_oscfx.py
+++ nts3kaoss_oscfx.py
@@ -2,7 +2,7 @@ from hvccloguesdk import LogueSDKV2Generator
 
 class Nts3_oscfx(LogueSDKV2Generator):
     FIXED_PARAMS = ("pitch",)
-    BUILTIN_PARAMS = ("pitch_note", "noteon_trig", "noteoff_trig")
+    BUILTIN_PARAMS = ("pitch_note", "touch_began", "touch_moved", "touch_ended", "touch_stationary", "touch_cancelled", "noteon_trig", "noteoff_trig")
     UNIT_NUM_OUTPUT = 1
     MAX_SDRAM_SIZE = 3145728
     MAX_UNIT_SIZE = 32768
